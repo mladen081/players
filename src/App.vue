16aeaaa647f3cb7720d62d8864525bc583bfc8c2
mladen081 <jovicmladen094@gmail.com>
2023-01-20 00:11:37 +0100

    003 Added Tailwind.

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,3 @@
-<script setup>
-import HelloWorld from "./components/HelloWorld.vue";
-</script>
-
 <template>
-  <div class="wrapper">
-    <HelloWorld msg="You did it!" />
-  </div>
+  <h1 class="mt-8 text-6xl text-center">Hello Vue</h1>
 </template>
---
+++ src/playground.js
@@ -0,0 +1,4 @@
+const fruits = ["Apple", "Banana", "Orange"];
+const vegetables = ["Cucunber", "Radish"];
+
+console.log([...fruits, ...vegetables]);
