aeee229e7278b677cf4cf4a2077786393117f00c
mladen081 <jovicmladen094@gmail.com>
2023-01-22 21:10:33 +0100

    009 Play II.

--- src/playground.js
+++ src/playground.js
@@ -1,4 +1,23 @@
-const fruits = ["Apple", "Banana", "Orange"];
-const vegetables = ["Cucunber", "Radish"];
+////////// 033 ES6 Spread Syntaks for Arrays
 
-console.log([...fruits, ...vegetables]);
+// const fruits = ["Apple", "Banana", "Orange"];
+// const vegetables = ["Cucunber", "Radish"];
+
+// console.log([...fruits, ...vegetables]);
+
+////////// 046 JavaScript Objects, Properties, and Methods
+
+const developer = {
+  salary: 100000,
+  experience: 4.5,
+  techStack: ["Vue", "HTML", "CSS"],
+  lookingForWork: true,
+  doubleSalary() {
+    this.salary = this.salary * 2;
+    this.lookingForWork = false;
+  },
+};
+
+console.log(developer);
+developer.doubleSalary();
+console.log(developer);
