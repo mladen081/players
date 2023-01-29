832fa5094aec8736fdd559141c2b304c2607e7ef
mladen081 <jovicmladen094@gmail.com>
2023-01-29 23:18:23 +0100

    013 Directives II start.

---
+++ src/components/ProfileImage.vue
@@ -0,0 +1,18 @@
+<template>
+  <img
+    clas="h-8 w-8 rounded-3xl object-contain"
+    :src="imageLink"
+    alt="Profile active"
+  />
+</template>
+
+<script>
+import { link } from "fs";
+
+export default {
+  name: "ProfileImage",
+  data() {
+    imageLink: "https://www.pngitem.com/pimgs/m/487-4876417_link-head-png-toon-link-face-png-transparent.png";
+  },
+};
+</script>
