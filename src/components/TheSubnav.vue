947e8d220d4d51d50992929e9dfa80a32cf340ce
mladen081 <jovicmladen094@gmail.com>
2023-02-09 22:13:26 +0100

    021 Subnav start.

---
+++ src/components/TheSubnav.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="h-16 w-full border-b border-solid border-brand-gray-1 bg-white">
+    <div class="flex h-full items-center px-8">This is a Subnav</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TheSubnav",
+};
+</script>
+
+<style></style>
