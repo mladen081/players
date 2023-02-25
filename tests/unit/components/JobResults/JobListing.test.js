8faf506eabb1aae66e8701b081033d8524dcfcc0
mladen081 <jovicmladen094@gmail.com>
2023-02-25 23:02:04 +0100

    030 Creating Mock Backend end.

--- tests/unit/components/JobResults/JobListing.test.js
+++ tests/unit/components/JobResults/JobListing.test.js
@@ -4,7 +4,15 @@ import { RouterLinkStub } from "@vue/test-utils";
 import JobListing from "@/components/JobResults/JobListing.vue";
 
 describe("JobListing", () => {
-  it("renders job title", () => {
+  const createJobProps = (jobProps = {}) => ({
+    title: "Vue Developer",
+    organization: "AirBnB",
+    locations: ["New York"],
+    minimumQualifications: ["Code"],
+    ...jobProps,
+  });
+
+  const renderJobListing = (jobProps) => {
     render(JobListing, {
       global: {
         stubs: {
@@ -13,26 +21,37 @@ describe("JobListing", () => {
       },
       props: {
         job: {
-          title: "Vue Developer",
+          ...jobProps,
         },
       },
     });
-    expect(screen.getByText("Vue Developer")).toBeInTheDocument();
+  };
+
+  it("renders job title", () => {
+    const jobProps = createJobProps({ title: "Vue Programmer" });
+    renderJobListing(jobProps);
+    expect(screen.getByText("Vue Programmer")).toBeInTheDocument();
   });
 
   it("renders job organization", () => {
-    render(JobListing, {
-      global: {
-        stubs: {
-          "router-link": RouterLinkStub,
-        },
-      },
-      props: {
-        job: {
-          organization: "AirBnB",
-        },
-      },
+    const jobProps = createJobProps({ organization: "Samsung" });
+    renderJobListing(jobProps);
+    expect(screen.getByText("Samsung")).toBeInTheDocument();
+  });
+
+  it("renders job locations", () => {
+    const jobProps = createJobProps({ locations: ["Orlando", "Jacksonville"] });
+    renderJobListing(jobProps);
+    expect(screen.getByText("Orlando")).toBeInTheDocument();
+    expect(screen.getByText("Jacksonville")).toBeInTheDocument();
+  });
+
+  it("renders job qualifications", () => {
+    const jobProps = createJobProps({
+      minimumQualifications: ["Code", "Develop"],
     });
-    expect(screen.getByText("AirBnB")).toBeInTheDocument();
+    renderJobListing(jobProps);
+    expect(screen.getByText("Code")).toBeInTheDocument();
+    expect(screen.getByText("Develop")).toBeInTheDocument();
   });
 });
